658c4520d33de9f13ee51e23725f7434d6069eda
Cem  Onal <mehmetcemonal@gmail.com>
2023-03-24 17:38:32 -0400

    basics are done

--- Fundamental Basics/index.ts
+++ Fundamental Basics/index.ts
@@ -28,18 +28,65 @@
 /* Classes */
 
 
-class OKGreeter {
+// class OKGreeter 
+// {
     
-    readonly id:number = 1983737472372
-    name: string;
+//     readonly id:number = 1983737472372
+//     name: string;
   
-     constructor(name: string)
-    {
-      this.name = name
-    } 
+//      constructor(name: string)
+//     {
+//       this.name = name
+//     } 
   
-  }
+// }
+
+
+
+// class OKGreeter
+// {
+//     static typeOfOrganization: string = 'Business'
+//     asd = `asd`
+
+//     constructor(public name:string,public hobby:string,protected secretWord:string, private secretPassword?:string)
+//     {
+
+//     }
+
+//     showName()
+//     {
+//         return this.secretPassword
+//     }
+// }
+
   
-  let objectOfClass = new OKGreeter('hua')
+//   let objectOfClass = new OKGreeter('JJ','ski','gizli','1313fg1')
   
-  console.log(objectOfClass) 
+//   console.log(objectOfClass) 
+
+
+
+
+
+/* Generics */
+
+const numbers = [42, 3, 77, 13, 104, 82];
+
+
+function sortItems<T = number, S = string>(items: T[],compareFn: (a: T, b: T) => number): (T | S)[] 
+{
+    return items.sort(compareFn)
+}
+  
+  
+const sortedNumbers = sortItems<number, string>(numbers,(a, b) => a - b);
+  
+console.log(sortedNumbers);
+  
+
+
+const names = ["CJ", "Trash_Dev", "RyanRoga", "loco86"];
+  
+const sortedNames = sortItems<string, number>(names,(a, b) => a.localeCompare(b),);
+  
+console.log(sortedNames);
