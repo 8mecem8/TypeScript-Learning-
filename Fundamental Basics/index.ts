0c91226d48fd965d25ac56b489abd1245cee500a
Cem  Onal <mehmetcemonal@gmail.com>
2023-03-24 15:43:40 -0400

    first steps

--- Fundamental Basics/index.ts
+++ Fundamental Basics/index.ts
@@ -1,5 +1,45 @@
-let count: number = 123123
-let aName: string = 'ocean'
-let aArray: (string | number)[] = [count,aName]
+/* Type Annotations */
 
-console.log(aArray)
+//let count: number = 123123
+//let aName: string = 'ocean'
+//let aArray: (string | number)[] = [count,aName] // Union Types
+
+//console.log(aArray)
+
+/* Inferred Types */
+
+
+
+// let mName = "MeceM";
+// let isM = true;
+// let aNumber = 422351;
+
+
+// const AFunc = (arg1: string, arg2: number) => 
+// {
+//     //return `this is ${arg1} and it is with ${arg2}`
+//     return  {arg1:arg2}
+// }
+
+
+
+
+
+/* Classes */
+
+
+class OKGreeter {
+    
+    readonly id:number = 1983737472372
+    name: string;
+  
+     constructor(name: string)
+    {
+      this.name = name
+    } 
+  
+  }
+  
+  let objectOfClass = new OKGreeter('hua')
+  
+  console.log(objectOfClass) 
